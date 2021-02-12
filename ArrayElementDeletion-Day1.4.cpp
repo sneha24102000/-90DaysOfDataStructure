49fcf03868270db243729d1a9fce983b8f341556
Sneha M <67436805+sneha24102000@users.noreply.github.com>
2021-02-12 21:34:49 +0530

    Add files via upload

---
+++ ArrayElementDeletion-Day1.4.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int arr[100]={9,19,29,39,49,59,69,79};
+	int key=49 , i=0,j=0,n=8;
+	
+	for(int i=0;i<n;i++)
+	    if(arr[i]==key)
+	        break;
+	        
+	for(int j=i;j<n-1;j++)
+	    arr[j]=arr[j+1];
+		
+	for(int i=0;i<n-1;i++)
+	    printf("\t%d\n",arr[i]);	
+		        
+}

84934ed710881b220f8abee57cb8fdd2424d2cdc
Sneha M <67436805+sneha24102000@users.noreply.github.com>
2021-03-05 22:32:04 +0530

    Add files via upload

---
+++ InsertingAtEndLL.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+typedef struct node
+{
+    int data;
+    struct node *next;
+}node;
+
+node *createNode()
+{
+    node *temp;
+    temp=(node*)malloc(sizeof(node));
+    return temp;
+}
+
+node *head;
+void insert()
+{
+    node *temp = createNode();
+    printf("Enter the data:");
+    scanf("%d",&temp->data);
+    temp->next=NULL;
+    if(head==NULL)
+       head=temp;
+    else{
+        node *ref=head;
+        while(ref->next != NULL)
+        {
+            ref=ref->next;
+            ref->next=temp;
+        }
+        
+        printf("Successfully inserted Data:\n");
+    }  
+    
+    void traverse(){
+        node *ref=head;
+        printf("\nData present in the list: \n");
+        while(ref!=NULL)
+        {
+            printf("%d\n",ref->data);
+            ref=ref->next;
+        }
+    }
+    
+}
+
+int main()
+{
+    
+    insert();
+    insert();
+    insert();
+    traverse();
+    
+    
+
+    return 0;
+}
